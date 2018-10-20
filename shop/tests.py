c023c42b50af901347d6b5f4cc827a441c443c3c
Touseef90 <touseef.ghazanfar@gmail.com>
2018-10-20 22:56:10 +0500

    Collaborative recommendation done

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pandas as pd
+import os
 
-# Create your tests here.
+class Recommend(object):
+
+    def suggest_products_for(self, product_id):
+        id = int(product_id)
+        PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+        file = os.path.join(PROJECT_PATH, 'predict1.csv')
+        prediction=pd.read_csv(file)
+        recoList=prediction[prediction.item1==id]\
+                    [["item2","score"]]\
+                    .sort_values("score", ascending=[0])
+        if recoList.empty:
+            return 'It is empty'
+        else:
+            return 'No dude not empty'
